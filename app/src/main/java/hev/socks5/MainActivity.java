58556ea523de894e140b84b2d5c32372445f263b
linzhikang <linzhikang2013@163.com>
2025-06-14 15:45:28 +0800

    update

--- app/src/main/java/hev/socks5/MainActivity.java
+++ app/src/main/java/hev/socks5/MainActivity.java
@@ -13,7 +13,6 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.Context;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
